12623d000fd387f7d994b92981570f5516e89718
Iris <iris.devillars@gmail.com>
2023-08-03 17:59:57 +0200

    fix: zoom btn color

--- components/UI/zoomButtons.tsx
+++ components/UI/zoomButtons.tsx
@@ -1,5 +1,5 @@
 import { Button, ButtonGroup } from "@mui/material";
-import React, { useState, useEffect, FunctionComponent } from "react";
+import React, { FunctionComponent } from "react";
 import AddIcon from "@mui/icons-material/Add";
 import RemoveIcon from "@mui/icons-material/Remove";
 
@@ -14,20 +14,33 @@ const ZoomButtons: FunctionComponent<ZoomButtonsProps> = ({
     <>
       <ButtonGroup
         variant="contained"
-        aria-label="outlined bg-backgroundLand button group"
+        aria-label="outlined button group"
         style={{
           position: "absolute",
           bottom: "10px",
           right: "10px",
+          backgroundColor: "#1a1528",
+          borderColor: "#1a1528",
         }}
       >
         <Button
-          className="bg-backgroundLand"
+          style={{
+            backgroundColor: "#1a1528",
+            borderColor: "#1a1528",
+          }}
+          variant="contained"
           onClick={() => handleMouseWheelProp(true)}
         >
           <AddIcon />
         </Button>
-        <Button onClick={() => handleMouseWheelProp(false)}>
+        <Button
+          style={{
+            backgroundColor: "#1a1528",
+            borderColor: "#1a1528",
+          }}
+          variant="contained"
+          onClick={() => handleMouseWheelProp(false)}
+        >
           <RemoveIcon />
         </Button>
       </ButtonGroup>
